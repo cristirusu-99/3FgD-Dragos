86cec04e753007a3740b32c7efe817c45b9f04d0
cristirusu-99 <59122086+cristirusu-99@users.noreply.github.com>
2020-10-07 21:24:29 +0300

    Initial commit

---
+++ Assets/scriptAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    public Vector3 zoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zoom = Camera.main.transform.position - GameObject.Find("Cactus").transform.position; 
+        if (zoom.y <= 0.2 && zoom.z >= -0.2)
+            animator.Play("Attack");
+    }
+}
